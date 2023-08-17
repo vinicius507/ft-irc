e3301f77307537aa4024887e0aca75372d59dae9
Vinicius Oliveira <vinicius@myriad.codes>
2023-08-17 18:03:05 -0300

    feat(nix): adds nix package output for ircserv

--- flake.nix
+++ flake.nix
@@ -18,6 +18,23 @@
           pkgs = nixpkgs.legacyPackages.${system};
         });
   in {
+    packages = forEachSystem ({pkgs}: {
+      default = self.packages.${pkgs.system}.ircserv;
+      ircserv = pkgs.stdenv.mkDerivation {
+        name = "ft-irc";
+        src = ./ft-irc;
+        nativeBuildInputs = with pkgs; [
+          llvmPackages_12.libcxxClang
+        ];
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ircserv $out/bin/ircserv
+        '';
+        meta = {
+          description = "A simple IRC Server written in C++ (aka WhatsApp 2).";
+        };
+      };
+    });
     devShells = forEachSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
