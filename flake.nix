b68db500203ebfdd14bbbf8db008609f55b82581
Vinicius Oliveira <vinicius@myriad.codes>
2023-09-06 10:07:17 -0300

    feat!(nix): adds 42-nix flake input
    BREAKING CHANGES: dropped support for aarch64-linux

--- flake.nix
+++ flake.nix
@@ -1,58 +1,44 @@
 {
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
+    ft-nix = {
+      url = "github:vinicius507/42-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    ft-nix,
   }: let
-    allSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-
-    mkShellOverlay = final: prev: {
-      mkShell = prev.mkShell.override {
-        inherit (final.llvmPackages_12) stdenv;
-      };
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [self.overlays.ircserv];
     };
-
-    forEachSystem = f:
-      nixpkgs.lib.genAttrs allSystems (system:
-        f {
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              self.overlays.ircserv
-              mkShellOverlay
-            ];
-          };
-        });
   in {
-    packages = forEachSystem ({pkgs}: {
-      default = self.packages.${pkgs.system}.ircserv;
+    packages.${system} = {
+      default = self.packages.${system}.ircserv;
       ircserv = import ./nix/pkgs/ircserv.nix {
         inherit (pkgs) lib;
         inherit (pkgs.llvmPackages_12) stdenv libcxxClang;
       };
-    });
+    };
     overlays = {
       default = self.overlays.ircserv;
       ircserv = final: _: {
         ircserv = self.packages.${final.system}.ircserv;
       };
     };
-    devShells = forEachSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          bear
-          clang-tools_12
-          gnumake
-          llvmPackages_12.libcxxClang
-          valgrind
-        ];
-      };
-    });
+    devShells.${system}.default = ft-nix.lib.mkShell {
+      packages = with pkgs; [
+        bear
+        clang-tools_12
+        gnumake
+        llvmPackages_12.libcxxClang
+        valgrind
+      ];
+    };
   };
 }
