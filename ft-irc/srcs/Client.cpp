6ddf209a8e912f051d7fafa54d4077668cea74b7
Vinicius Oliveira <vinicius@myriad.codes>
2023-09-15 16:13:40 -0300

    chore: fix _username typo and renames client methods

--- ft-irc/srcs/Client.cpp
+++ ft-irc/srcs/Client.cpp
@@ -1,23 +1,23 @@
 #include "Client.hpp"
 #include <string>
 
-Client::Client(void) : _nickname(), _usarname() {}
+Client::Client(void) : _nickname(), _username() {}
 
 Client::Client(const std::string nickname, const std::string username)
-    : _nickname(nickname), _usarname(username) {}
+    : _nickname(nickname), _username(username) {}
 
 Client::Client(const Client &client) { *this = client; }
 
 Client &Client::operator=(const Client &client) {
   if (this != &client) {
     this->_nickname = client._nickname;
-    this->_usarname = client._usarname;
+    this->_username = client._username;
   }
   return (*this);
 }
 
 Client::~Client(void) {}
 
-std::string Client::getName(void) { return (this->_usarname); }
+std::string Client::getNickName(void) { return (this->_nickname); }
 
-std::string Client::getNick(void) { return (this->_nickname); }
+std::string Client::getUserName(void) { return (this->_username); }
--- ft-irc/srcs/Client.hpp
+++ ft-irc/srcs/Client.hpp
@@ -4,20 +4,24 @@
 #include <string>
 
 class Client {
-
 public:
   Client(void);
+
   Client(const std::string nickname, const std::string username);
+
   Client(const Client &client);
-  Client &operator=(const Client &client);
+
   ~Client(void);
 
-  std::string getNick(void);
-  std::string getName(void);
+  Client &operator=(const Client &client);
+
+  std::string getNickName(void);
+
+  std::string getUserName(void);
 
 private:
   std::string _nickname;
-  std::string _usarname;
+  std::string _username;
 };
 
 #endif
