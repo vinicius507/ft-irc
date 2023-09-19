ef8ff358cb75f8c1eed0a94b3ec31fcd441367eb
Vinicius Oliveira <vinicius@myriad.codes>
2023-09-19 13:18:40 -0300

    chore: formatting

--- ft-irc/srcs/Server.cpp
+++ ft-irc/srcs/Server.cpp
@@ -9,91 +9,73 @@
 
 bool Server::_shouldExit = false;
 
-Server::Server(void) : _pollFds(), _socket()
-{
-}
-Server::Server(short port) : _pollFds(), _socket(port)
-{
+Server::Server(void) : _pollFds(), _socket() {}
+Server::Server(short port) : _pollFds(), _socket(port) {}
+
+Server::Server(const Server &other)
+    : _pollFds(other._pollFds), _socket(other._socket) {}
+
+Server::~Server(void) {}
+
+Server &Server::operator=(const Server &other) {
+  if (this != &other) {
+    this->_pollFds = other._pollFds;
+    this->_socket = other._socket;
+  }
+  return (*this);
 }
 
-Server::Server(const Server &other) : _pollFds(other._pollFds), _socket(other._socket)
-{
-}
+bool Server::run(void) {
+  std::vector<int> arrayOfFds;
+  std::vector<int>::iterator itOfFds;
 
-Server::~Server(void)
-{
-}
-
-Server &Server::operator=(const Server &other)
-{
-    if (this != &other)
-    {
-        this->_pollFds = other._pollFds;
-        this->_socket = other._socket;
+  if (!this->_socket.bindAndListen()) {
+    std::cerr << "Error: failed to create server TCP socket." << std::endl;
+    return (false);
+  }
+  this->_pollFds.addFd(this->_socket.getFd());
+  while (this->_shouldExit != true) {
+    switch (this->_pollFds.poll()) {
+    case PERROR:
+      return (false);
+    case PTIMEOUT:
+      continue;
+    default:
+      break;
     }
-    return (*this);
+    if (this->_pollFds.hasNewClientOnQueue()) {
+      this->_pollFds.addFd(this->_socket.acceptClient());
+    }
+    arrayOfFds = this->_pollFds.getFdsReadyForReading();
+    for (itOfFds = arrayOfFds.begin(); itOfFds != arrayOfFds.end(); ++itOfFds) {
+      this->handleClientData(*itOfFds);
+    }
+  }
+  return (true);
 }
 
-bool Server::run(void)
-{
-    std::vector<int> arrayOfFds;
-    std::vector<int>::iterator itOfFds;
+void Server::handleClientData(int clientFd) {
+  int bytesRead;
+  char buffer[255] = {0};
 
-    if (!this->_socket.bindAndListen())
-    {
-        std::cerr << "Error: failed to create server TCP socket." << std::endl;
-        return (false);
-    }
-    this->_pollFds.addFd(this->_socket.getFd());
-    while (this->_shouldExit != true)
-    {
-        switch (this->_pollFds.poll())
-        {
-            case PERROR:
-                return (false);
-            case PTIMEOUT:
-                continue;
-            default:
-                break;
-        }
-        if (this->_pollFds.hasNewClientOnQueue())
-        {
-            this->_pollFds.addFd(this->_socket.acceptClient());
-        }
-        arrayOfFds = this->_pollFds.getFdsReadyForReading();
-        for (itOfFds = arrayOfFds.begin(); itOfFds != arrayOfFds.end(); ++itOfFds)
-        {
-            this->handleClientData(*itOfFds);
-        }
-    }
-    return (true);
+  bytesRead = ::recv(clientFd, buffer, sizeof(buffer), MSG_DONTWAIT);
+  if (bytesRead == -1) {
+    std::perror("recv");
+    this->_pollFds.removeFd(clientFd);
+    return;
+  }
+  if (bytesRead == 0) {
+    std::cerr << "Client disconnected: " << clientFd << std::endl;
+    this->_pollFds.removeFd(clientFd);
+    return;
+  }
+  std::string msg(buffer, buffer + bytesRead);
+  std::cerr << "Message from client: " << msg << std::endl;
+  ::send(clientFd, "\n", 1, 0);
 }
 
-void Server::handleClientData(int clientFd)
-{
-    int bytesRead;
-    char buffer[255] = {0};
-
-    bytesRead = ::recv(clientFd, buffer, sizeof(buffer), MSG_DONTWAIT);
-    if (bytesRead == -1)
-    {
-        std::perror("recv");
-        this->_pollFds.removeFd(clientFd);
-        return;
-    }
-    if (bytesRead == 0)
-    {
-        std::cerr << "Client disconnected: " << clientFd << std::endl;
-        this->_pollFds.removeFd(clientFd);
-        return;
-    }
-    std::string msg(buffer, buffer + bytesRead);
-    std::cerr << "Message from client: " << msg << std::endl;
-    ::send(clientFd, "\n", 1, 0);
-}
-
-void Server::gracefulShutdown(int signal)
-{
-    Server::_shouldExit = true;
-    std::cerr << "\nReceived signal " << signal << ", shutting down the server." << std::endl;
+void Server::gracefulShutdown(int signal) {
+  Server::_shouldExit = true;
+  std::cerr << "\nReceived signal " << signal << ", shutting down the server."
+            << std::endl;
 }
--- ft-irc/srcs/Server.hpp
+++ ft-irc/srcs/Server.hpp
@@ -4,24 +4,23 @@
 #include "PollManager.hpp"
 #include "ServerSocket.hpp"
 
-class Server
-{
-  public:
-    Server(void);
-    Server(short port);
-    Server(const Server &other);
-    ~Server(void);
-    Server &operator=(const Server &other);
+class Server {
+public:
+  Server(void);
+  Server(short port);
+  Server(const Server &other);
+  ~Server(void);
+  Server &operator=(const Server &other);
 
-    bool run(void);
-    void handleClientData(int fd);
+  bool run(void);
+  void handleClientData(int fd);
 
-    static void gracefulShutdown(int signal);
-    static bool _shouldExit;
+  static void gracefulShutdown(int signal);
+  static bool _shouldExit;
 
-  private:
-    PollManager _pollFds;
-    ServerSocket _socket;
+private:
+  PollManager _pollFds;
+  ServerSocket _socket;
 };
 
 #endif
